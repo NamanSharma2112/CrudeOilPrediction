0def912a3fe1549b2ecd5d47d459c7dcebfa9bb9
Naman Sharma <namansharmans03@gmail.com>
2026-03-27 13:47:50 +0530

    commit

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,59 +1,116 @@
-import React from 'react'
-import {Home, Inbox, Calendar, Search, Settings, Sidebar} from "lucide-react"
-import { SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
-import Link from 'next/link'
+import { Calendar, ChevronUp, Home, Inbox, Search, Settings, User2 } from "lucide-react"
+
+import { 
+  Sidebar, 
+  SidebarContent, 
+  SidebarFooter, 
+  SidebarGroup, 
+  SidebarGroupContent, 
+  SidebarGroupLabel, 
+  SidebarHeader, 
+  SidebarMenu, 
+  SidebarMenuButton, 
+  SidebarMenuItem, 
+  SidebarSeparator
+} from "./ui/sidebar"
+import Link from "next/link"
+import Image from "next/image"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
+
 const items = [
-  {title:"Home",
-    url:"/",
-    icon:Home,
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
   },
-{
-  title:"Inbox",
-  url:"/inbox",
-  icon:Inbox,
-},
-{
-  title:"Calendar",
-  url:"/calendar",
-  icon:Calendar,
-},{
-  title:"Search",
-  url:"/search",
-  icon:Search,
-},
-{
-  title:"Settings",
-  url:"/settings",
-  icon:Settings,
-},
-];
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  }
+]
+
 const AppSidebar = () => {
   return (
-  <Sidebar>
-      <SidebarHeader> </SidebarHeader>
-<SidebarContent>
-<SidebarGroup>
-  <SidebarGroupLabel>  Application </SidebarGroupLabel>
-  <SidebarGroupContent>
-    <SidebarMenu>
-      {items.map(item=>(
-        <SidebarMenuItem key={item.title}>
-<SidebarMenuButton asChild>
-  <Link href={item.url}>
-  <item.icon/>
-  <span>{item.title}</span>
-  </Link>
-</SidebarMenuButton>
-        </SidebarMenuItem>
-      ))}
-    </SidebarMenu>
-  </SidebarGroupContent>
-</SidebarGroup>
-</SidebarContent>
-<SidebarFooter></SidebarFooter>
-
+    <Sidebar>
+      <SidebarHeader className="mt-3" >
+        <SidebarMenu>
+          <SidebarMenuItem>
+             <SidebarMenuButton asChild>
+              <Link href="/">
+              <Image src="https://github.com/shadcn.png" alt="logo" width={30} height={30} className="rounded-full object-cover "/>
+              <span>Legion Dev</span>
+              </Link>
+             </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+         </SidebarHeader>
+         <SidebarSeparator></SidebarSeparator>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title} >
+                  <SidebarMenuButton asChild >
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 />
+                  <span>John Doe</span>
+                
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+            
+              <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width] transition-all duration-300 ease-in-out">
+                <DropdownMenuItem>
+                  <span>Profile</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+              
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
-  );
-};
+  )
+}
 
 export default AppSidebar
